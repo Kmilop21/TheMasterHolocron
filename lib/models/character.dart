e4b85559f528f6f46eeb3212205b9a2ce4d9571c
Camilo <camilo.e.provoste.p@gmail.com>
2024-11-30 23:46:42 -0300

    dependencias para el provider

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -1,16 +1,32 @@
 class Character {
+  final String id;
   final String name;
   final String description;
   final String image;
 
-  Character(
-      {required this.name, required this.description, required this.image});
+  Character({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.image,
+  });
 
+  // To convert a Character to/from JSON
   factory Character.fromJson(Map<String, dynamic> json) {
     return Character(
+      id: json['_id'],
       name: json['name'],
       description: json['description'],
       image: json['image'],
     );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      '_id': id,
+      'name': name,
+      'description': description,
+      'image': image,
+    };
   }
 }
